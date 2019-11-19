232470f57d1dd1713a7714d9ef3c96d767db9ecb
ghdus4185 <ckdghdus@naver.com>
2019-11-19 14:10:38 +0900

    todo list

--- TODO-PROJECT/todo-back/todos/urls.py
+++ TODO-PROJECT/todo-back/todos/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
   # localhost:8000/api/v1/todos
   path('todos/', views.todo_create),
-  # localhost:8000/api
+  # localhost:8000/api/v1/todos/10/
   path('todos/<int:id>/', views.todo_detail),
+  path('users/<int:id>/', views.user_detail),
 ]
